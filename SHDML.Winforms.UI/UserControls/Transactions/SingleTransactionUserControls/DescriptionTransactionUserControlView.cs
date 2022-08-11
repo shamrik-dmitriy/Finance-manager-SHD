e38d1180a4551305b8cdc75a6ef490fc18ee5201
Шамрик Дмитрий Леонидович <dshamrick@gems.local>
2022-08-11 10:10:36 +0600

    + Добавил контрол для описания

---
+++ SHDML.Winforms.UI/UserControls/Transactions/SingleTransactionUserControls/DescriptionTransactionUserControlView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SHDML.Winforms.UI.UserControls.Transactions.SingleTransactionUserControls
+{
+    public partial class DescriptionTransactionUserControlView : UserControl
+    {
+        public DescriptionTransactionUserControlView()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxDescriptionTransaction_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
