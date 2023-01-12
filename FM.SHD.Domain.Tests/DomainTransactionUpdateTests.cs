4b2d99613177cd6f0c13500557894f2654749ad2
Шамрик Дмитрий Леонидович <dshamrick@gemsdev.com>
2023-01-12 17:31:32 +0600

    + Добавил тест для проверки сумм

--- FM.SHD.Domain.Tests/DomainTransactionUpdateTests.cs
+++ FM.SHD.Domain.Tests/DomainTransactionUpdateTests.cs
@@ -10,13 +10,38 @@ namespace FM.SHD.Domain.Tests
 {
     public class DomainTransactionUpdateTests
     {
+        private IAccountServices _accountServices;
+
+        public DomainTransactionUpdateTests()
+        {
+            _accountServices = new MockRepository(MockBehavior.Default).Of<IAccountServices>()
+                .Where(services => services.GetById(It.Is<long>(id => id == 1)) == new AccountDto()
+                {
+                    Id = 1,
+                    CurrentSum = -10
+                })
+                .Where(services => services.GetById(It.Is<long>(id => id == 2)) == new AccountDto()
+                {
+                    Id = 2,
+                    CurrentSum = 10
+                })
+                .Where(services => services.GetById(It.Is<long>(id => id == 3)) == new AccountDto()
+                {
+                    Id = 3,
+                    CurrentSum = 0
+                })
+                .First();
+        }
+
         #region Тестирование изменения сумм
 
+        #region Без изменения типа транзакции
+
         /// <summary>
-        ///     Расход - Доход. Тестирование суммы
+        ///     Расход.
         /// </summary>
         [Fact]
-        public void DomainTransaction_OnUpdate_TransactionTypeHasChanged_ExpenseToIncome_CheckSum()
+        public void DomainTransaction_OnUpdate_TransactionTypeHasNotChanged_Expense_CheckSum()
         {
             var mockTransactionServices = new MockRepository(MockBehavior.Default).Of<ITransactionServices>()
                 .Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
@@ -31,6 +56,32 @@ namespace FM.SHD.Domain.Tests
                     IdentityId = 1,
                     CreditAccountId = null,
                     DebitAccountId = 1,
+                    TypeTransactionId = 1
+                }).Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
+                {
+                    Id = 2,
+                    Name = "Молоко Лужайкино ультр. 2.5%",
+                    Description = "Молоко ультрапастеризованное, 2.5%",
+                    Date = DateTime.Now,
+                    Sum = (decimal)69.99,
+                    CategoryId = 1,
+                    ContragentId = 1,
+                    IdentityId = 1,
+                    CreditAccountId = null,
+                    DebitAccountId = 2,
+                    TypeTransactionId = 1
+                }).Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
+                {
+                    Id = 2,
+                    Name = "Молоко Лужайкино ультр. 2.5%",
+                    Description = "Молоко ультрапастеризованное, 2.5%",
+                    Date = DateTime.Now,
+                    Sum = (decimal)69.99,
+                    CategoryId = 1,
+                    ContragentId = 1,
+                    IdentityId = 1,
+                    CreditAccountId = null,
+                    DebitAccountId = 3,
                     TypeTransactionId = 1
                 })
                 .First();
@@ -71,11 +122,164 @@ namespace FM.SHD.Domain.Tests
             var transactionDomain = new TransactionsDomain(mockTransactionServices, mockAccountServices);
 
             var updatedTransaction = transactionDomain.OnUpdateTransaction(newTransaction);
-            
+
             Assert.True(updatedTransaction.Sum == 30);
-            Assert.True(mockAccountServices.GetById(1).CurrentSum == (decimal) -49.99);
-            //Assert.True(mockAccountServices.GetById(1).CurrentSum == (decimal) -79.99);
+            Assert.True(mockAccountServices.GetById(1).CurrentSum == (decimal)29.99);
+
+            newTransaction = new TransactionDto()
+            {
+                Id = 1,
+                Name = "Молоко Лужайкино ультр. 2.5%",
+                Description = "Молоко ультрапастеризованное, 2.5%",
+                Date = DateTime.Now,
+                Sum = 30,
+                CategoryId = 1,
+                ContragentId = 1,
+                IdentityId = 1,
+                CreditAccountId = null,
+                DebitAccountId = 2,
+                TypeTransactionId = 1
+            };
+
+            transactionDomain.OnUpdateTransaction(newTransaction);
+
+            Assert.True(mockAccountServices.GetById(2).CurrentSum == (decimal)49.99);
+
+            newTransaction = new TransactionDto()
+            {
+                Id = 1,
+                Name = "Молоко Лужайкино ультр. 2.5%",
+                Description = "Молоко ультрапастеризованное, 2.5%",
+                Date = DateTime.Now,
+                Sum = 30,
+                CategoryId = 1,
+                ContragentId = 1,
+                IdentityId = 1,
+                CreditAccountId = null,
+                DebitAccountId = 3,
+                TypeTransactionId = 1
+            };
+
+            transactionDomain.OnUpdateTransaction(newTransaction);
+
+            Assert.True(mockAccountServices.GetById(3).CurrentSum == (decimal)39.99);
         }
+
+        /// <summary>
+        ///     Доход
+        /// </summary>
+        [Fact]
+        public void DomainTransaction_OnUpdate_TransactionTypeHasNotChanged_Income_CheckSum()
+        {
+            var mockTransactionServices = new MockRepository(MockBehavior.Default).Of<ITransactionServices>()
+                .Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
+                {
+                    Id = 1,
+                    Name = "Сушилка для овощей и фруктов Supra",
+                    Description = "Продал на Авито",
+                    Date = DateTime.Now,
+                    Sum = (decimal)500,
+                    CategoryId = 1,
+                    ContragentId = 1,
+                    IdentityId = 1,
+                    CreditAccountId = 1,
+                    DebitAccountId = null,
+                    TypeTransactionId = 2
+                }).Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
+                {
+                    Id = 2,
+                    Name = "Гладильная доска Nika",
+                    Description = "Продал на Авито",
+                    Date = DateTime.Now,
+                    Sum = (decimal)240,
+                    CategoryId = 1,
+                    ContragentId = 1,
+                    IdentityId = 1,
+                    CreditAccountId = 2,
+                    DebitAccountId = null,
+                    TypeTransactionId = 2
+                }).Where(x => x.GetById(It.Is<long>(id => id == 1)) == new TransactionDto()
+                {
+                    Id = 2,
+                    Name = "Масло автомобильное 5W-30",
+                    Description = "Сдал в магазин остатки",
+                    Date = DateTime.Now,
+                    Sum = (decimal)320.65,
+                    CategoryId = 1,
+                    ContragentId = 1,
+                    IdentityId = 1,
+                    CreditAccountId = 3,
+                    DebitAccountId = null,
+                    TypeTransactionId = 2
+                })
+                .First();
+
+            var newTransaction = new TransactionDto()
+            {
+                Id = 1,
+                Name = "Молоко Лужайкино ультр. 2.5%",
+                Description = "Молоко ультрапастеризованное, 2.5%",
+                Date = DateTime.Now,
+                Sum = 30,
+                CategoryId = 1,
+                ContragentId = 1,
+                IdentityId = 1,
+                CreditAccountId = 1,
+                DebitAccountId =  null,
+                TypeTransactionId = 2
+            };
+
+            var transactionDomain = new TransactionsDomain(mockTransactionServices, _accountServices);
+
+            var updatedTransaction = transactionDomain.OnUpdateTransaction(newTransaction);
+
+            Assert.True(updatedTransaction.Sum == 30);
+            Assert.True(_accountServices.GetById(1).CurrentSum == (decimal)-300.65);
+
+            newTransaction = new TransactionDto()
+            {
+                Id = 1,
+                Name = "Молоко Лужайкино ультр. 2.5%",
+                Description = "Молоко ультрапастеризованное, 2.5%",
+                Date = DateTime.Now,
+                Sum = 30,
+                CategoryId = 1,
+                ContragentId = 1,
+                IdentityId = 1,
+                CreditAccountId = 2,
+                DebitAccountId =  null,
+                TypeTransactionId = 2
+            };
+
+            transactionDomain.OnUpdateTransaction(newTransaction);
+
+            Assert.True(_accountServices.GetById(2).CurrentSum == (decimal)49.99);
+
+            newTransaction = new TransactionDto()
+            {
+                Id = 1,
+                Name = "Молоко Лужайкино ультр. 2.5%",
+                Description = "Молоко ультрапастеризованное, 2.5%",
+                Date = DateTime.Now,
+                Sum = 30,
+                CategoryId = 1,
+                ContragentId = 1,
+                IdentityId = 1,
+                CreditAccountId = 3,
+                DebitAccountId = null,
+                TypeTransactionId = 2
+            };
+
+            transactionDomain.OnUpdateTransaction(newTransaction);
+
+            Assert.True(_accountServices.GetById(3).CurrentSum == (decimal)39.99);
+        }
+
+        #endregion
+
+        #region С Изменением типа транзакции
+
+        #endregion
 
         #endregion
 
