0dc09c246fc94f5029d9d5910b27af93c306fd10
Шамрик Дмитрий Леонидович <dshamrick@gemsdev.com>
2023-01-30 14:12:41 +0600

    + Добавлены заготовки для проектов модулей

---
+++ FM.SHD.Plugin.Transaction/TransactionPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using FM.SHD.Plugins.Infrastructure;
+
+namespace FM.SHD.Plugin.Transaction
+{
+    public class TransactionPlugin : IPlugin
+    {
+        private string _connectionString;
+        
+        public string Name => "Плагин транзакций";
+        public string Id => "Transaction";
+        public string Description => "Плагин добавляет функциональность для работы с транзакциями";
+        public bool IsAddDataToTab => true;
+        public bool IsAddDataToMenu => false;
+
+        public TransactionPlugin()
+        {
+        }
+
+        public void SetConnectionString(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public TransactionPlugin(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public TabPage GetTab()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToolStripMenuItem GetMenuItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FM.SHD.Plugins.Infrastructure/IPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FM.SHD.Plugins.Infrastructure
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+        string Description { get; }
+        string Id { get; }
+
+        bool IsAddDataToTab { get; }
+        bool IsAddDataToMenu { get; }
+
+        TabPage GetTab();
+
+        ToolStripMenuItem GetMenuItem();
+    }
+}
