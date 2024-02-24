0f9004d358ec5719811bd282ead249d455a848cd
shamrik-dmitriy <@fschrga+hfvr2pa>
2024-02-24 23:05:45 +0600

    + Добавил перечисление для типов категорий

---
+++ FM.SHD.Domain/Models/Categories/TypeOfCategory.cs
@@ -0,0 +1,9 @@
+namespace FM.SHD.Domain.Models.Categories
+{
+    public enum TypeOfCategory
+    {
+        ProductCategory = 1,
+        AccountCategory = 2,
+        ContragentCategory = 3
+    }
+}
